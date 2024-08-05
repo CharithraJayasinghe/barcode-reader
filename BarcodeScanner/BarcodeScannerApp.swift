1ef5957b1f16a7d0180d4c2c2eb7f161623bb7ff
CharithraJayasinghe <charithraseneth3@gmail.com>
2024-08-05 14:25:57 +0530

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Charithra Jayasingha on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Charithra Jayasingha on 2024-08-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
